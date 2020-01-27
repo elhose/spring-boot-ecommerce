2feef4bbbad0180aabedd219ff8ec993c249dfe0
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-01-27 16:28:56 +0100

    fixing repo

--- src/main/java/tt/psc/overhead/springbootecommerce/entity/Product.java
+++ src/main/java/tt/psc/overhead/springbootecommerce/entity/Product.java
@@ -1,4 +1,35 @@
 package tt.psc.overhead.springbootecommerce.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tt.psc.overhead.springbootecommerce.entity.names.ProductNames;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = ProductNames.TABLENAME)
+@Data
+@NoArgsConstructor
 public class Product {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ProductNames.ID)
+    private Long id;
+
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private Boolean active;
+    private Integer unitsInStock;
+    private Date dateCreated;
+    private Date dateUpdated;
+    private Integer categoryId;
+
+
+    
 }
--- src/main/java/tt/psc/overhead/springbootecommerce/entity/names/ProductNames.java
+++ src/main/java/tt/psc/overhead/springbootecommerce/entity/names/ProductNames.java
@@ -1,4 +1,18 @@
-package tt.psc.overhead.springbootecommerce.entity;
+package tt.psc.overhead.springbootecommerce.entity.names;
 
-public class NamesProduct {
+public class ProductNames {
+    public static final String TABLENAME = "product";
+    public static final String ID = "id";
+    public static final String SKU = "sku";
+    public static final String NAME = "name";
+    public static final String DESCRIPTION = "description";
+    public static final String UNIT_PRICE = "unit_price";
+    public static final String IMAGE_URL = "image_url";
+    public static final String ACTIVE = "active";
+    public static final String UNITS_IN_STOCK = "units_in_stock";
+    public static final String DATE_CREATED = "date_created";
+    public static final String LAST_UPDATED = "last_updated";
+
+    private ProductNames() {
+    }
 }
