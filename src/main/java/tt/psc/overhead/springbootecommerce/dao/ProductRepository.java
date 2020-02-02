f983f6d71edc544f83f7b2708b4c91d81b5ab362
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-02-02 23:55:44 +0100

    added table of products - it uses angular service that calls REST API

--- src/main/java/tt/psc/overhead/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/tt/psc/overhead/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,10 @@
 package tt.psc.overhead.springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import tt.psc.overhead.springbootecommerce.entity.Product;
 
-@Repository
+//@Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
